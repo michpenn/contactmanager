c71b316057bbbd3ccb0a94244059bace652407aa
michpenn <michpenn91@gmail.com>
2018-10-14 16:20:58 -0400

    Updates

---
+++ precache-manifest.4e273c2f29ec4e3ba7d04284e9d6b7b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "1097af3450d18901c16d",
+    "url": "/contactmanager/static/js/main.1097af34.chunk.js"
+  },
+  {
+    "revision": "2e06f80dada12f04b070",
+    "url": "/contactmanager/static/js/1.2e06f80d.chunk.js"
+  },
+  {
+    "revision": "1097af3450d18901c16d",
+    "url": "/contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "2e06f80dada12f04b070",
+    "url": "/contactmanager/static/css/1.8465f582.chunk.css"
+  },
+  {
+    "revision": "3526975e27a8b66efd6980c1e89ef67a",
+    "url": "/contactmanager/index.html"
+  }
+];
